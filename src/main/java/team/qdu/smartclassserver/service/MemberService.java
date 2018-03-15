a2b428b0c397dab952627bf0c2cc87e906ccb143
karuao <1160263495@qq.com>
2018-03-16 00:49:26 +0800

    新增部分签到功能

--- src/main/java/team/qdu/smartclassserver/service/MemberService.java
+++ src/main/java/team/qdu/smartclassserver/service/MemberService.java
@@ -29,4 +29,34 @@ public class MemberService {
         String jsonResponse = new Gson().toJson(apiResponse);
         return jsonResponse;
     }
+
+    //获取用户班课列表
+    public String getMemberInfo(int classUserId){
+        ClassUser cu = classUserMapper.selectMemberInfoByPrimaryKey(classUserId);
+        ApiResponse<ClassUser> apiResponse;
+        if(cu!=null){
+            //该用户存在
+            apiResponse = new ApiResponse<>("0","成员存在");
+            apiResponse.setObj(cu);
+        }else{
+            //该用户不存在
+            apiResponse = new ApiResponse<>("2", "成员不存在");
+        }
+        String jsonResponse = new Gson().toJson(apiResponse);
+
+        return jsonResponse;
+    }
+
+    //移出班课
+    public String shiftClass(int classUserId) {
+        ApiResponse apiResponse;
+        int result = classUserMapper.deleteByPrimaryKey(classUserId);
+        if (result == 1) {
+            apiResponse = new ApiResponse("0", "已成功移出班课");
+        } else {
+            apiResponse = new ApiResponse("1", "移出班课失败");
+        }
+        String jsonResponse = new Gson().toJson(apiResponse);
+        return jsonResponse;
+    }
 }
